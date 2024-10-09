75d30ada8b9a56881606e56dcadf79a745b3815e
Tom <sckzyo@gmail.com>
2024-10-10 00:37:13 +0200

    update slurm_binary_info

--- slurm_binary_info.go
+++ slurm_binary_info.go
@@ -1,19 +1,4 @@
-/* Copyright 2024 Thomas Bourcey
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>. */
-
-/* Copyright 2024 Tom
+/* Copyright 2017 Thomas Bourcey
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -38,61 +23,62 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 )
 
-// SlurmBinariesCollector collects metrics for Slurm binaries and their versions.
-type SlurmBinariesCollector struct {
-	binaryInfo *prometheus.Desc
-	binaries   []string
+// SlurmInfoCollector defines a Prometheus collector for Slurm binary and version information
+type SlurmInfoCollector struct {
+	slurmInfo *prometheus.Desc
+	binaries  []string
 }
 
-// NewSlurmBinariesCollector initializes a new SlurmBinariesCollector with a list of Slurm binaries.
-func NewSlurmBinariesCollector() *SlurmBinariesCollector {
-	// List of all main Slurm binaries to check
+// NewSlurmInfoCollector initializes a new SlurmInfoCollector
+func NewSlurmInfoCollector() *SlurmInfoCollector {
 	binaries := []string{
 		"sinfo", "squeue", "sdiag", "scontrol",
 		"sacct", "sbatch", "salloc", "srun",
 	}
-	// Metric description with labels: type="binary", binary_name="name", version="version"
-	return &SlurmBinariesCollector{
-		binaryInfo: prometheus.NewDesc("slurm_binary_info", "Information on Slurm binaries presence and versions", []string{"binary", "version"}, nil),
-		binaries:   binaries,
+	labels := []string{"type", "binary", "version"}
+	return &SlurmInfoCollector{
+		slurmInfo: prometheus.NewDesc("slurm_info", "Information on Slurm version and binaries", labels, nil),
+		binaries:  binaries,
 	}
 }
 
-// Describe sends the metric descriptions to the Prometheus channel.
-func (c *SlurmBinariesCollector) Describe(ch chan<- *prometheus.Desc) {
-	ch <- c.binaryInfo
+// Describe sends the metric descriptions to Prometheus
+func (c *SlurmInfoCollector) Describe(ch chan<- *prometheus.Desc) {
+	ch <- c.slurmInfo
 }
 
-// Collect gathers metrics for each Slurm binary.
-func (c *SlurmBinariesCollector) Collect(ch chan<- prometheus.Metric) {
+// Collect gathers the Slurm information and sends it as a metric to Prometheus
+func (c *SlurmInfoCollector) Collect(ch chan<- prometheus.Metric) {
+	// Get the general Slurm version
+	version, found := GetBinaryVersion("sinfo")
+	versionValue := 0.0
+	if found {
+		versionValue = 1.0
+	}
+	// Send the general Slurm version as a metric
+	ch <- prometheus.MustNewConstMetric(c.slurmInfo, prometheus.GaugeValue, versionValue, "general", "", version)
+
+	// Check each binary and send their availability and version
 	for _, binary := range c.binaries {
-		version, found := GetBinaryVersion(binary)
-		binaryValue := 0.0
-		if found {
-			binaryValue = 1.0
+		binVersion, binFound := GetBinaryVersion(binary)
+		binValue := 0.0
+		if binFound {
+			binValue = 1.0
 		}
-		ch <- prometheus.MustNewConstMetric(c.binaryInfo, prometheus.GaugeValue, binaryValue, binary, version)
+		ch <- prometheus.MustNewConstMetric(c.slurmInfo, prometheus.GaugeValue, binValue, "binary", binary, binVersion)
 	}
 }
 
-// GetBinaryVersion checks if a Slurm binary is installed and returns its version string.
+// getBinaryVersion checks if a Slurm binary is installed and returns its version string
 func GetBinaryVersion(binary string) (string, bool) {
-	// Check if the binary is available in PATH
-	_, err := exec.LookPath(binary)
-	if err != nil {
-		log.Printf("Binary %s not found in PATH", binary)
-		return "not_found", false
-	}
-
-	// Get the version of the binary using `--version` option
 	cmd := exec.Command(binary, "--version")
 	output, err := cmd.Output()
 	if err != nil {
-		log.Printf("Error executing %s: %v", binary, err)
+		log.Printf("Binary %s not found: %v", binary, err)
 		return "not_found", false
 	}
 
-	// Extract the version number, e.g., "slurm 23.11.6"
+	// Extract the version number from the output, e.g., "slurm 23.11.6"
 	fields := strings.Fields(string(output))
 	if len(fields) > 1 {
 		return fields[1], true
